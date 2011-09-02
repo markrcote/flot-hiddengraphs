09738e27b6a9a390ef900059465e9649ccb47fb3
William Lachance <wlach@mozilla.com>
2011-09-02 10:43:10 -0400

    Add myself as a contributor

--- jquery.flot.hiddengraphs.js
+++ jquery.flot.hiddengraphs.js
@@ -20,6 +20,7 @@
  *
  * Contributor(s):
  *     Mark Cote <mcote@mozilla.com>
+ *     William Lachance <wlachance@mozilla.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
