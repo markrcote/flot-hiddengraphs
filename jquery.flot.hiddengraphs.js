fea2d7b5bece45f54b032892807a598c4d87e72c
Mark Côté <mcote@alumni.uwaterloo.ca>
2014-05-04 13:30:48 -0400

    Support for real-time graphs; patch by rore.
    This was adapted from pull request #7, unbitrotted and with a few
    formatting and other minor fixes.

--- jquery.flot.hiddengraphs.js
+++ jquery.flot.hiddengraphs.js
@@ -38,9 +38,10 @@
  */
 (function ($) {
     var options = { };
-    var drawnOnce = false;
 
     function init(plot) {
+        var drawnOnce = false;
+
         function findPlotSeries(label) {
             var plotdata = plot.getData();
             for (var i = 0; i < plotdata.length; i++) {
@@ -57,13 +58,16 @@
                 return;
             }
 
+            var options = plot.getOptions();
             var switchedOff = false;
+
             if (typeof series.points.oldShow === "undefined") {
                 series.points.oldShow = false;
             }
             if (typeof series.lines.oldShow === "undefined") {
                 series.lines.oldShow = false;
             }
+
             if (series.points.show && !series.points.oldShow) {
                 series.points.show = false;
                 series.points.oldShow = true;
@@ -74,11 +78,14 @@
                 series.lines.oldShow = true;
                 switchedOff = true;
             }
+
             if (switchedOff) {
                 series.oldColor = series.color;
                 series.color = "#fff";
+                setHidden(options, label, true);
             } else {
                 var switchedOn = false;
+
                 if (!series.points.show && series.points.oldShow) {
                     series.points.show = true;
                     series.points.oldShow = false;
@@ -89,8 +96,10 @@
                     series.lines.oldShow = false;
                     switchedOn = true;
                 }
+
                 if (switchedOn) {
             	    series.color = series.oldColor;
+                    setHidden(options, label, false);
             	}
             }
 
@@ -101,13 +110,45 @@
             plot.draw();
         }
 
-        function plotLabelHandlers(plot, options) {
-            $(".graphlabel").mouseenter(function() { $(this).css("cursor", "pointer"); })
-                            .mouseleave(function() { $(this).css("cursor", "default"); })
-                            .unbind("click").click(function() { plotLabelClicked($(this).parent().text()); });
-	    $(".legendColorBox").mouseenter(function() { $(this).css("cursor", "pointer"); })
-		    		.mouseleave(function() { $(this).css("cursor", "default"); })
-				.unbind("click").click(function() { plotLabelClicked($(this).parent().text()); });
+        function setHidden(options, label, hide) {
+            // Record state to a new variable in the legend option object.
+            if (!options.legend.hidden) {
+                options.legend.hidden = [];
+            }
+
+            var pos = options.legend.hidden.indexOf(label);
+
+            if (hide) {
+                if (pos < 0) {
+                    options.legend.hidden.push(label);
+                }
+            } else {
+                if (pos > -1) {
+                    options.legend.hidden.splice(pos, 1);
+                }
+            }
+        }
+
+        function setHideAction(elem) {
+            elem.mouseenter(function() { $(this).css("cursor", "pointer"); })
+                .mouseleave(function() { $(this).css("cursor", "default"); })
+                .unbind("click").click(function() {
+                    plotLabelClicked($(this).parent().text());
+                });
+        }
+
+        function plotLabelHandlers(plot) {
+            var options = plot.getOptions();
+
+            if (!options.legend.hideable) {
+                return;
+            }
+
+            var p = plot.getPlaceholder();
+
+            setHideAction(p.find(".graphlabel"));
+            setHideAction(p.find(".legendColorBox"));
+
             if (!drawnOnce) {
                 drawnOnce = true;
                 if (options.legend.hidden) {
@@ -126,26 +167,46 @@
             options.legend.labelFormatter = function(label, series) {
                 return '<span class="graphlabel">' + label + '</span>';
             };
-
-            // Really just needed for initial draw; the mouse-enter/leave
-            // functions will call plotLabelHandlers() directly, since they
-            // only call setupGrid().
-            plot.hooks.draw.push(function (plot, ctx) {
-                plotLabelHandlers(plot, options);
-            });
         }
 
-        plot.hooks.processOptions.push(checkOptions);
-
         function hideDatapointsIfNecessary(plot, s, datapoints) {
-            if (!plot.getOptions().legend.hideable) {
+            var options = plot.getOptions();
+
+            if (!options.legend.hideable) {
                 return;
+            }
+
+            if (options.legend.hidden &&
+                options.legend.hidden.indexOf(s.label) > -1) {
+                var off = false;
+
+                if (s.points.show) {
+                    s.points.show = false;
+                    s.points.oldShow = true;
+                    off = true;
+                }
+                if (s.lines.show) {
+                    s.lines.show = false;
+                    s.lines.oldShow = true;
+                    off = true;
+                }
+
+                if (off) {
+                    s.oldColor = s.color;
+                    s.color = "#fff";
+                }
             }
 
             if (!s.points.show && !s.lines.show) {
                 s.datapoints.format = [ null, null ];
             }
         }
+
+        plot.hooks.processOptions.push(checkOptions);
+
+        plot.hooks.draw.push(function (plot, ctx) {
+            plotLabelHandlers(plot);
+        });
 
         plot.hooks.processDatapoints.push(hideDatapointsIfNecessary);
     }
