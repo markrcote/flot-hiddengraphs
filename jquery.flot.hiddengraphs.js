9ab56881d64bba8c9a9f33118f53ce3297df9679
Andrei Prislopski <prislop@gmail.com>
2015-04-20 11:26:54 -0700

    fix legend container

--- jquery.flot.hiddengraphs.js
+++ jquery.flot.hiddengraphs.js
@@ -153,7 +153,7 @@
                 return;
             }
 
-            var p = plot.getPlaceholder();
+            var p = options.legend.container ? $(options.legend.container) : plot.getPlaceholder();
 
             setHideAction(p.find(".graphlabel"));
             setHideAction(p.find(".legendColorBox"));
